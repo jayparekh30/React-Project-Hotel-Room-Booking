162a2a90a3780bda2eb3e92e33ddbcf49828e7dd
Jaykumar Parekh <jayparekhx4@.com>
2024-02-10 10:32:55 +0530

    initial

--- react-beach-resort-dev/src/Components/Footer/Footer.jsx
+++ react-beach-resort-dev/src/Components/Footer/Footer.jsx
@@ -6,17 +6,6 @@ export default function Footer() {
   return (
     <footer className="footer">
       <h2>Beach Resort | Hotel Room Book</h2>
-      <p>
-        &copy; <span>{year}</span> BeachResort.com - All Rights Copyright
-        Reserved To{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.facebook.com/SamiurRahmanMukul"
-        >
-          Samiur Rahman Mukul
-        </a>
-      </p>
     </footer>
   );
 }
